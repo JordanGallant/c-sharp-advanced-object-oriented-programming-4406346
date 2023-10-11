bb5447c4b7775e3bf87c60437865e9a9cb9b570e
Jordan Gallant <76824916+JordanGallant@users.noreply.github.com>
2023-10-11 04:55:35 +0000

    constructors

--- Start/Ch1/DefineClasses/shapes.cs
+++ Start/Ch1/DefineClasses/shapes.cs
@@ -2,18 +2,27 @@
 // Defining and instantiating classes
 
 // TODO: Define a class using the "class" keyword
-class shapes {
-    
-}
+class Rectangle {
+    // TODO: member variables hold data
+public int w;
+public in h;
+
 
     // TODO: The constructor accepts parameters used to create the object
-
+    public Rectangle(int w, int h){
+        this.w =w;
+        this.h=h;
+    }
 
     // TODO: For convenience, we can have a constructor that takes one value
     // for squares that have the same side size
+    public Rectangle(int side){
+        this.w =this.h = side;
+    }
 
 
     // TODO: Classes can define methods that return values
 
 
-    // TODO: member variables hold data
+    
+}
